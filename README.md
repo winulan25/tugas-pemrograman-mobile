b35ceab1d017fd63d168177104d7cad953f10546
winulan25 <winky1525.00@gmail.com>
2020-11-21 15:37:06 +0800

    tugas pemrograman mobile

---
+++ README.md
@@ -0,0 +1,11 @@
+## Kucing & Anjing Pedia | v 2
+
+Contoh aplikasi android sederhana dengan 2 activity yang mendemonstrasikan:
+* Cara mengirim informasi dari satu activity ke activity lainnya melalui extra pada Intent, 
+* Penggunaan kode yang lebih efisien dari contoh sebelumnya dengan menggunakan inheritance pada model
+
+Merupakan kelanjutan dari contoh sebelumnya di [Kucing & Anjing Pedia](https://github.com/ewinsutriandi/kucing-anjing-pedia)
+
+![Kucing & Anjing Pedia](catdogpedia.gif)
+
+
---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,93 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+import simple.example.hewanpedia.model.Burung;
+
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+        kucings.add(new Kucing("Siam", "Thailand",
+                "Kucing hasil turunan dari Wichian Maat (Kucing Thailand) dengan ciri khas berbadan panjang dan langsing, wajah lonjong, telinga lebar, dan mata kebiruan", R.drawable.cat_siam));
+        kucings.add(new Kucing("Siberia", "Rusia",
+                "Kucing domestik Rusia dengan bulu lebat dan badan besar tapi sangat lincah dan terkenal dengan kemampuan melompatnya ", R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+        anjings.add(new Anjing("Shepherd", "Jerman",
+                "Anjing pintar dan kuat, sekarang banyak digunakan sebagai anjing penjaga, pengawal, dan anjing polisi (K9)", R.drawable.dog_shepherd));
+        anjings.add(new Anjing("Shiba", "Jepang",
+                "Anjing dari daerah Shiba ini gesit dan lincah sehingga awalnya banyak digunakan untuk berburu ", R.drawable.dog_shiba));
+        return anjings;
+    }
+    private static List<Burung> initDataBurung(Context ctx) {
+        List<Burung> burungs = new ArrayList<>();
+        burungs.add(new Burung("jalak", "kalimantan",
+                "burung yang umumnya berukuran sedang (sekitar 20-25 cm) gagah, dengan paruh yang kuat, tajam dan lurus;", R.drawable.burung_jalak));
+        burungs.add(new Burung("ciblek", "Indonesia ",
+                "burung kecil ramping dengan panjang total (diukur dari ujung paruh hingga ujung ekor) sekitar 13cm. hampir seluruh sisi atas badan berwarna coklat hijau-zaitun. tenggorokan dan dada putih, perut dan pantat kekuningan", R.drawable.burung_ciblek));
+        burungs.add(new Burung("cucuk rawa", "Indonesia",
+                "burung yang berukuran sedang, panjang tubuh total (diukur dari ujung paruh hingga ujung ekor) sekitar 28cm.", R.drawable.burung_cucukrawa));
+        burungs.add(new Burung("pleci", "jawa",
+                "panjang tubuhnya (diukur dari ujung paruh hingga ujung ekor) berkisar antara 8-15cm.ciri yang paling jelas adalah adanya lingkaran putih yang mengelilingi mata,meskipun ada beberapa jenisnya yang tidak memilikinya.", R.drawable.burun_pleci));
+        burungs.add(new Burung("kenari", "jawa",
+                "burung kecil yang bersuara merdu dan berbulu kuning yang indah", R.drawable.burung_kenari));
+        burungs.add(new Burung("kecer", "indonesia",
+                "burung ini berwarna hitam dan putih dengan ekor yang panjang. ekornya terangkat ke atas jika mereka sedang mencari makanan ditanah atau kadang ketika sedang bertengger", R.drawable.burung_kecer));
+        return burungs;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(initDataBurung(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
